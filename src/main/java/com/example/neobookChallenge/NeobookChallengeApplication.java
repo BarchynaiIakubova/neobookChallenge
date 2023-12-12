fb3b62aa8d771a04f791c272237059d07c5681fa
Barchynai <barchynai.iakubova@gmail.com>
2023-12-12 18:30:07 +0600

    Intro

---
+++ src/main/java/com/example/neobookChallenge/NeobookChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.example.neobookChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobookChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobookChallengeApplication.class, args);
+		System.out.println("Welcome to Eco Market project");
+	}
+
+}
---
+++ src/test/java/com/example/neobookChallenge/NeobookChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neobookChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobookChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
